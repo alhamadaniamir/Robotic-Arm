322729a3b9434c5feead1dc5d296cd3c113d1233
alhamadaniamir <123335433+alhamadaniamir@users.noreply.github.com>
2026-07-02 21:08:29 +0800

    Coorected pins

--- robotic_armpractice.ino
+++ robotic_armpractice.ino
@@ -82,7 +82,6 @@
 //   // Pause before repeating
 //   delay(2000);
 // }
-
 #include <Servo.h>
 #include <AccelStepper.h>
 
@@ -95,21 +94,99 @@ Servo s1, s2, s3, s4, s5, s6;
 
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
-void setup() {
+// -------------------------------------------------
+// Smooth Servo Movement Function
+// -------------------------------------------------
+void smoothMove(Servo &servo, int startAngle, int endAngle, int speedDelay)
+{
+  if (startAngle < endAngle)
+  {
+    for (int pos = startAngle; pos <= endAngle; pos++)
+    {
+      servo.write(pos);
+      delay(speedDelay);
+    }
+  }
+  else
+  {
+    for (int pos = startAngle; pos >= endAngle; pos--)
+    {
+      servo.write(pos);
+      delay(speedDelay);
+    }
+  }
+}
+
+// -------------------------------------------------
+// Test One Servo
+// -------------------------------------------------
+void testServo(Servo &servo, const char *name)
+{
+  Serial.print("Testing ");
+  Serial.println(name);
+
+  // Move from 0° to 90°
+  smoothMove(servo, 0, 90, 20);
+  delay(1000);
+
+  // Move back from 90° to 0°
+  smoothMove(servo, 90, 0, 20);
+  delay(1000);
+}
+
+// -------------------------------------------------
+// Test Stepper Motor
+// -------------------------------------------------
+void testStepper()
+{
+  Serial.println("Testing Stepper");
+
+  digitalWrite(EN_PIN, LOW); // Enable stepper
+
+  // Move forward
+  stepper.moveTo(800);
+  while (stepper.distanceToGo() != 0)
+  {
+    stepper.run();
+  }
+
+  delay(1000);
+
+  // Move back
+  stepper.moveTo(0);
+  while (stepper.distanceToGo() != 0)
+  {
+    stepper.run();
+  }
+
+  delay(1000);
+
+  digitalWrite(EN_PIN, HIGH); // Disable stepper
+}
+
+// -------------------------------------------------
+// Setup
+// -------------------------------------------------
+void setup()
+{
+  Serial.begin(115200);
 
   // Stepper setup
   pinMode(EN_PIN, OUTPUT);
-  digitalWrite(EN_PIN, HIGH);   // Disable stepper
+  digitalWrite(EN_PIN, HIGH); // Disable stepper initially
 
-  stepper.setMaxSpeed(1000);
+  stepper.setMaxSpeed(600);
+  stepper.setAcceleration(300);
 
   // Attach servos
   s1.attach(5);   // ELBOW
-  s2.attach(6);   // wrist
-  s3.attach(7);   // gripper
-  s4.attach(8);   // BASE_1
+  s2.attach(6);   // WRIST
+  s3.attach(7);   // GRIPPER
+  s4.attach(10);  // BASE_1
   s5.attach(9);   // BASE_2
-  s6.attach(10);  // SHOULDER
+  s6.attach(8);   // SHOULDER
+  
+ 
 
   // Initialize all servos to 0°
   s1.write(0);
@@ -119,9 +196,22 @@ void setup() {
   s5.write(0);
   s6.write(0);
 
-  delay(2000);    // Give the servos time to move to 0°
+  delay(3000);
 }
 
-void loop() {
+// -------------------------------------------------
+// Main Loop
+// -------------------------------------------------
+void loop()
+{
+  testServo(s1, "ELBOW");
+  testServo(s2, "WRIST");
+  testServo(s3, "GRIPPER");
+  testServo(s6, "SHOULDER");
+  testServo(s5, "BASE_2");
+  testServo(s4, "BASE_1");
 
+  testStepper();
+
+  delay(3000);
 }
